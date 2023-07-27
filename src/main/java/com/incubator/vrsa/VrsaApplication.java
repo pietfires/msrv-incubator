2da8949d74d687c28e1f4672570349956dcd1496
Kerev Reddy <kerevreddy@hotmail.com>
2023-07-27 10:20:02 +0200

    Initial commit

---
+++ src/main/java/com/incubator/vrsa/VrsaApplication.java
@@ -0,0 +1,13 @@
+package com.incubator.vrsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incubator/vrsa/VrsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubator.vrsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
