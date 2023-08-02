0d880ce656792ebc7f8a0843ca4ba296968de210
RevTheDevZA <kerev.reddy@entelect.co.za>
2023-08-02 08:23:54 +0200

    Update HazelcastConfiguration.java and the application.yml to cater for ttl injection

--- src/main/java/com/incubator/vrsa/config/HazelcastConfiguration.java
+++ src/main/java/com/incubator/vrsa/config/HazelcastConfiguration.java
@@ -1,11 +1,14 @@
 package com.incubator.vrsa.config;
 
 import com.hazelcast.config.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HazelcastConfiguration {
+    @Value("${hazelcast.movies.ttl}")
+    private int timeToLiveSeconds;
     @Bean
     public Config hazelcastConfig() {
         return new Config()
@@ -19,7 +22,7 @@ public class HazelcastConfiguration {
                                                 .setMaxSizePolicy(MaxSizePolicy.FREE_HEAP_SIZE)
                                                 .setSize(200)
                                 )
-                                .setTimeToLiveSeconds(0)
+                                .setTimeToLiveSeconds(timeToLiveSeconds)
                 );
     }
 }
